7c9a77fcf05c48eb07844915ea3e6edcff5ca5e5
Clavijomo <jonathanclavijo0907@gmail.com>
2024-10-11 01:43:22 -0500

    update

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose';
 
 export const ConnectMongoDB = () => {
-    mongoose.connect("mongodb+srv://clavijomo:12345@prueba.nuapb.mongodb.net/", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-        .then(() => {
-            console.log('Connected to MongoDB');
+
+    try {
+        mongoose.connect("mongodb+srv://clavijomo:12345@prueba.nuapb.mongodb.net/", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
         })
-        .catch((error) => {
-            console.error('Error connecting to MongoDB', error);
-        });
+        console.log("DB CONECTADA")
+    } catch (error) {
+        console.log(error)
+        process.exit(1);
+    }
 }
